0e02b9df18ae84b01c04c8bd592a4e7145aaa9a4
alain.tchougbo <alain.tchougbo@epitech.eu>
2025-02-05 18:26:45 +0100

    Add facebook card

--- script.js
+++ script.js
@@ -15,35 +15,35 @@ document.getElementById("shareButton").addEventListener("click", () => {
   });
   
 
-  document.getElementById('addContactButton').addEventListener('click', function (e) {
-    e.preventDefault();
+  // document.getElementById('addContactButton').addEventListener('click', function (e) {
+  //   e.preventDefault();
   
-    // Contenu de la vCard
-    const vCardData = `
-  BEGIN:VCARD
-  VERSION:3.0
-  FN:Digital Business International Academy
-  TEL:+2290141202282
-  EMAIL:learnatdbia@gmail.com
-  ORG:Digital Business International Academy
-  ADR:Abomey-Calavi
-  END:VCARD
-    `;
+  //   // Contenu de la vCard
+  //   const vCardData = `
+  // BEGIN:VCARD
+  // VERSION:3.0
+  // FN:Digital Business International Academy
+  // TEL:+2290141202282
+  // EMAIL:learnatdbia@gmail.com
+  // ORG:Digital Business International Academy
+  // ADR:Abomey-Calavi
+  // END:VCARD
+  //   `;
   
-    // Création d'un fichier Blob
-    const blob = new Blob([vCardData], { type: 'text/vcard' });
-    const url = URL.createObjectURL(blob);
+  //   // Création d'un fichier Blob
+  //   const blob = new Blob([vCardData], { type: 'text/vcard' });
+  //   const url = URL.createObjectURL(blob);
   
-    // Création d'un lien pour télécharger le fichier
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = 'DBIA_Contact.vcf'; // Nom du fichier téléchargé
-    document.body.appendChild(a);
-    a.click();
+  //   // Création d'un lien pour télécharger le fichier
+  //   const a = document.createElement('a');
+  //   a.href = url;
+  //   a.download = 'DBIA_Contact.vcf'; // Nom du fichier téléchargé
+  //   document.body.appendChild(a);
+  //   a.click();
   
-    // Nettoyage
-    document.body.removeChild(a);
-    URL.revokeObjectURL(url);
-  });
+  //   // Nettoyage
+  //   document.body.removeChild(a);
+  //   URL.revokeObjectURL(url);
+  // });
   
   
